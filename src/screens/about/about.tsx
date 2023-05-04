f010e44f3029a8b6fdc99935c9c77945e125a4e9
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-05-04 10:54:38 -0300

    feat(about): base screen

--- src/screens/about/about.tsx
+++ src/screens/about/about.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
 import PageView from '../../components/page-view/pageview';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
 
 const About = (): JSX.Element => {
-  return <PageView />;
+  return (
+    <PageView>
+      <View style={styles.versionWrapper}>
+        <Text style={styles.version}>v1.0.0</Text>
+      </View>
+    </PageView>
+  );
 };
 
 export default About;
