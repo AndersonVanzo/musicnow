8ec947bfa4f800d33a988bdb14161cf5468a9141
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-05-05 15:40:23 -0300

    fix(eslint): removed no-control-regex rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
   extends: '@react-native-community',
   rules: {
     'object-curly-spacing': 0,
+    'no-control-regex': 0,
     'react/no-unstable-nested-components': ['off', { allowAsProps: true }],
   },
 };
