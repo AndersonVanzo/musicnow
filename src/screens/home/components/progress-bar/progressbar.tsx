64e635d73b8e6936b38cf0894897e02e4368ada9
Anderson R. Vanzo <anderson.vanzo01@gmail.com>
2023-05-05 15:37:56 -0300

    fix(player): better time formatting

--- src/screens/home/components/progress-bar/progressbar.tsx
+++ src/screens/home/components/progress-bar/progressbar.tsx
@@ -3,17 +3,10 @@ import { Text, View } from 'react-native';
 import { styles } from './styles';
 import usePlayerContext from '../../../../hooks/usePlayerContext';
 import Animated from 'react-native-reanimated';
+import { formatSeconds } from '../../../../utils/formatSeconds';
 
 const ProgressBar = (): JSX.Element => {
   const { progress, progressBarWidth } = usePlayerContext();
-
-  const formatTime = (seconds: number): string => {
-    const minutes = Math.floor(seconds / 60);
-    const remainingSeconds = Math.round(seconds % 60);
-    const formattedMinutes = String(minutes).padStart(2, '0');
-    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
-    return `${formattedMinutes}:${formattedSeconds}`;
-  };
 
   return (
     <View style={styles.container}>
@@ -22,8 +15,8 @@ const ProgressBar = (): JSX.Element => {
         <Animated.View style={[styles.bar, styles.filledBar, progressBarWidth]} />
       </View>
       <View style={styles.timeWrapper}>
-        <Text style={styles.time}>{formatTime(progress.position)}</Text>
-        <Text style={styles.time}>{formatTime(progress.duration)}</Text>
+        <Text style={styles.time}>{formatSeconds(progress.position)}</Text>
+        <Text style={styles.time}>{formatSeconds(progress.duration)}</Text>
       </View>
     </View>
   );
