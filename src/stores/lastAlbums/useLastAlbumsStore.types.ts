5df6ceae2ebd1d33913c1c58113173da31cca6a9
AndersonVanzo <anderson.vanzo01@gmail.com>
2024-03-06 21:43:04 -0300

    refactor(zustand): removed lastAlbums store

--- src/stores/lastAlbums/useLastAlbumsStore.types.ts
+++
@@ -1,7 +0,0 @@
-import { AlbumProps } from "../../screens/library/Library.types.ts";
-
-export type LastAlbumsStoreProps = {
-    lastAlbums: AlbumProps[];
-    setLastAlbums: (data: AlbumProps[]) => void;
-    addLastAlbum: (album: AlbumProps) => void;
-};
